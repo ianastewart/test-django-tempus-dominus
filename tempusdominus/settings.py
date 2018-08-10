ba38ca9720a6a5d942e887642679173834ddc95d
ian <is@ktconsultants.co.uk>
2018-08-10 16:57:15 +0100

    Adjusted Settings

--- tempusdominus/settings.py
+++ tempusdominus/settings.py
@@ -115,32 +115,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-# # LIST: https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
-# DATE_FORMAT = 'd/m/Y'
-# TIME_FORMAT = 'H:i'
-# DATETIME_FORMAT = 'd/m/Y H:i'
-# YEAR_MONTH_FORMAT = 'F Y'
-# MONTH_DAY_FORMAT = 'F j'
-# SHORT_DATE_FORMAT = 'd/m/Y'
-# SHORT_DATETIME_FORMAT = 'd/m/Y H:i'
-# FIRST_DAY_OF_WEEK = 1
-#
-# # BUT here use the Python strftime format syntax,
-# # LIST: http://docs.python.org/library/datetime.html#strftime-strptime-behavior
-#
-# DATE_INPUT_FORMATS = (
-#     '%d/%m/%Y',  # '21/03/2014'
-#     '%d-%m-%Y',  # '21-03-2014'
-#     '%d/%m/%y',  # '21/03/14'
-# )
-# TIME_INPUT_FORMATS = (
-#     '%H:%M:%S',  # '17:59:59'
-#     '%H:%M',  # '17:59'
-# )
-#
-# DATETIME_INPUT_FORMATS = (
-#     '%d-%m-%Y %H:%M',  # '21-03-2014 17:59'
-# )
+
+DATE_INPUT_FORMATS = (
+    '%d/%m/%Y',  # '21/03/2014'
+    '%d-%m-%Y',  # '21-03-2014'
+    '%d/%m/%y',  # '21/03/14'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',  # '17:59:59'
+    '%H:%M',  # '17:59'
+)
+
+DATETIME_INPUT_FORMATS = (
+    '%d-%m-%Y %H:%M',  # '21-03-2014 17:59'
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
